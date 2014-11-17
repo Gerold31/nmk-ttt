b6a9868884581cbdacc507a217f8b680cec43073
Gerold31 <kramer.leroy@gmail.com>
2014-11-17 13:26:08 +0100

    Fix local game

--- nmk-ttt/gamelocal.cpp
+++ nmk-ttt/gamelocal.cpp
@@ -16,22 +16,34 @@ void GameLocal::run()
     uint winner = 0;
     uint nextPlayer = 0;
     uint c[mN];
+    uint s[mK];
+    for(uint i=0; i<mK; i++)
+    {
+        uint j;
+        mNmk->addPlayer(QString().setNum(i), j, s[i]);
+    }
     do
     {
-        nextPlayer++;
+        nmk::ERROR e = nmk::ERROR::NONE;
         do
         {
-            std::cout << "Player " << nextPlayer << "'s turn" << std::endl;
+            if(e != nmk::ERROR::NONE)
+            {
+                std::cout << "Error " << (int)e << std::endl;
+            }
+            std::cout << "Player " << nextPlayer+1 << "'s turn" << std::endl;
 
             for(size_t i=0; i<mN; i++)
             {
                 std::cin >> c[i];
             }
-        }while(mNmk->turn(c, nextPlayer) != nmk::ERROR::NONE);
+        }while((e=mNmk->turn(c, s[nextPlayer])) != nmk::ERROR::NONE);
+
+        nextPlayer++;
 
         if(nextPlayer >= mK)
             nextPlayer = 0;
-    }while(mNmk->getWinner() == 0);
+    }while((winner = mNmk->getWinner()) == 0);
 
     if(winner == (uint)-1)
         std::cout << "draw" << std::endl;
